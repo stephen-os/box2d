ce3d7e6ebcbd010371215c6d71ca53cfe872894d
stephen-os <ImStephen.1sw@gmail.com>
2025-08-06 20:22:41 -0400

    Updated premake5 to include multithreading

--- premake5.lua
+++ premake5.lua
@@ -1,47 +1,81 @@
 project "Box2D"
-	kind "StaticLib"
-	language "C"
-	staticruntime "off"
-	
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
-	
-	files
-	{
-		"src/**.h",
-		"src/**.c",
-		"include/**.h"
-	}
-	
-	includedirs
-	{
-		"include",
-		"src"
-	}
-	
-	defines
-	{
-		"_CRT_SECURE_NO_WARNINGS"
-	}
-	
-	filter "system:windows"
-		systemversion "latest"
-		buildoptions { "/std:c11", "/experimental:c11atomics" }
-		
-	filter "system:linux"
-		pic "On"
-		systemversion "latest"
-		buildoptions { "-std=c11" }
-		
-	filter "configurations:Debug"
-		runtime "Debug"
-		symbols "on"
-		
-	filter "configurations:Release"
-		runtime "Release"
-		optimize "on"
-		
-	filter "configurations:Dist"
-		runtime "Release"
-		optimize "on"
-		symbols "off"
+    kind "StaticLib"
+    language "C"
+    staticruntime "off"
+    
+    -- Enable multi-core compilation
+    flags { "MultiProcessorCompile" }
+    
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+    
+    files
+    {
+        "src/**.h",
+        "src/**.c",
+        "include/**.h"
+    }
+    
+    includedirs
+    {
+        "include",
+        "src"
+    }
+    
+    -- Platform-specific settings
+    filter "system:windows"
+        systemversion "latest"
+        
+        defines
+        {
+            "_CRT_SECURE_NO_WARNINGS"
+        }
+        
+        buildoptions 
+        { 
+            "/std:c11", 
+            "/experimental:c11atomics" 
+        }
+        
+    filter "system:linux"
+        pic "On"
+        systemversion "latest"
+        
+        buildoptions 
+        { 
+            "-std=c11" 
+        }
+        
+        -- Linux math library linking
+        links
+        {
+            "m"  -- Math library for C math functions
+        }
+        
+    filter "system:macosx"
+        systemversion "latest"
+        
+        buildoptions 
+        { 
+            "-std=c11" 
+        }
+    
+    -- Configuration-specific settings
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "On"
+        defines { "DEBUG" }
+        
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "On"
+        defines { "NDEBUG" }
+        
+    filter "configurations:Dist"
+        runtime "Release"
+        optimize "On"
+        symbols "Off"
+        defines { "NDEBUG", "DIST_BUILD" }
+        
+    -- Clear filters
+    filter {}
